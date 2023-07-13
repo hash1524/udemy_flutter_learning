2d10416d849ace5d53ad0baaefb7221002f4f4d2
bobby <bobbycodes1524@gmail.com>
2023-07-13 23:26:33 +0530

    learned abt bacckground color argument of scaffold and Color object

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 54, 38, 105),
         body: Center(
           child: Text('Hello World!'),
         ),
